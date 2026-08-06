3b499b96481f4f0df3a619af581d88e142a28888
Chieh <secret12501250@gmail.com>
2026-08-06 17:13:23 +0800

    feat: add DisplayTodoProps type to DisplayTodo

--- src/components/Todo/DisplayTodo.tsx
+++ src/components/Todo/DisplayTodo.tsx
@@ -1,4 +1,9 @@
-function DisplayTodo({ content, completed }) {
+type DisplayTodoProps = {
+  content: string;
+  completed: boolean;
+};
+
+function DisplayTodo({ content, completed }: DisplayTodoProps) {
   return (
     <span
       className={`flex-1 text-body leading-normal py-2 px-2.5 border border-transparent transition-colors ${completed ? 'text-gray-400 line-through' : 'text-gray-900'}`}
